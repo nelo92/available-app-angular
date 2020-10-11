1ba4e76fe168b2864091af2664789a05110d5fee
manuel <nelo92@hotmail.com>
2020-10-11 19:38:06 +0200

    conf build prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
+    version: require('../../package.json').version,    
+    firebase: {
+        apiKey: "AIzaSyBtShpfIxWdclulPpCQ-BN9XybkuRuEzuw",
+        authDomain: "available-app-5071d.firebaseapp.com",
+        databaseURL: "https://available-app-5071d.firebaseio.com",
+        projectId: "available-app-5071d",
+        storageBucket: "available-app-5071d.appspot.com",
+        messagingSenderId: "702468920612",
+        appId: "1:702468920612:web:e02f06a1f2684cdd730fb1",
+        measurementId: "G-8LC34REC3L"
+      },
     production: true
 }
